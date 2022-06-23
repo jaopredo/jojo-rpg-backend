c6dd12e47e20184c008af95c24b80101cad48b39
jaopredo <joao.profissional.45@gmail.com>
2022-06-22 21:52:39 -0300

    vai

--- src/controllers/standController.js
+++ src/controllers/standController.js
@@ -1,5 +1,14 @@
 const router = require('express').Router()
+const cors = require('cors')
 
+const corsConfig = {
+    origin: [ "https://jojo-rpg-frontend.herokuapp.com" ],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type', 'authorization']
+}
+
+router.use(cors(corsConfig))
 
 /* SCHEMAS */
 const Stand = require('../database/schemas/StandSchema')
@@ -14,5 +23,7 @@ router.get('/', masterAuth, charValidation, async (req, res) => {
     const stand = await Stand.findOne({ playerId: req.id })
     return res.json(stand);
 })
+
+app.options("*", cors(corsConfig))
 
 module.exports = app => app.use('/stand', router)
