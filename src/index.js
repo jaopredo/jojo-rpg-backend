4a7677b94b19179df523d9c166949067200037d4
jaopredo <joao.profissional.45@gmail.com>
2022-06-22 21:19:52 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors({ origin: '*' }))
+app.use(cors({ origin: 'https://jojo-rpg-frontend.herokuapp.com' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
