530960ff88dad898e19dc50f67015a83379a7d9a
jaopredo <joao.profissional.45@gmail.com>
2022-06-26 00:49:08 -0300

    n aguento mais

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express()
 const corsConfig = {
     origin: [
         "https://jojo-rpg-frontend.herokuapp.com",
-        "http://jojo-rpg-dm.herokuapp.com",
+        "https://jojo-rpg-dm.herokuapp.com",
         // "http://localhost:3000"
     ],
     credentials: true,
