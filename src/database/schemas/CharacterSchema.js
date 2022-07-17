e047eef5b8d9a445c8bc3c6bd37c11df8e673bf9
jaopredo <joao.profissional.45@gmail.com>
2022-07-17 16:41:24 -0300

    corrigindo bug

--- src/database/schemas/CharacterSchema.js
+++ src/database/schemas/CharacterSchema.js
@@ -157,52 +157,8 @@ CharacterSchema.pre('updateOne', async function(next) {
     // Separando atributos e especialidades
     const { attributes, specialitys } = updateObj
     if (attributes || specialitys) {
-        const {
-            strengh: strenghSpecs,  // Especialidades de Força
-            dexterity: dexSpecs, // Especialidades de Destreza
-            constituition: constSpecs,  // Especialidades de Constituição
-            vigillance: vigSpecs, // Especialidades de Vigilância
-        } = specialitys
-
-        // Calculando Vida
-        const life = calcLife(
-            attributes.constituition,  // Passo minha constituição
-            constSpecs?.painResistence,
-            constSpecs?.imunity,
-            constSpecs?.force,
-        )
-        // Nota: A exclamação serve para indicar que, se constSpecs não for UNDEFINED, eu passo o atributo 'painResistence'
-
-        // Calculando Energia Mental
-        const mentalEnergy = calcMentalEnergy(
-            attributes.constituition,
-            strenghSpecs?.mindResistence,
-            constSpecs?.force
-        )
-
-        // Calculando Dificuldade do Acerto
-        const successDifficult = calcSuccessDifficult(
-            attributes.dexterity,
-            dexSpecs?.dodge,
-            vigSpecs?.reflex
-        )
-
-        // Calculando Movimento
-        const movement = calcMovement(
-            attributes.strengh,
-            attributes.dexterity,
-            strenghSpecs?.athletics,
-            strenghSpecs?.jump
-        )
-
         this.set({  // Coloco o novo objeto dentro do Schema a ser salvo
-            combat: {
-                life: life,
-                mentalEnergy: mentalEnergy,
-                movement: movement,
-                da: successDifficult,
-                shield: 0
-            },
+            combat: makeCombatObject(attributes, specialitys),
             updatedAt: new Date()
         })
     } else {
